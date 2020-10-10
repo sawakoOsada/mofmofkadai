f3cef62d6133465e5a507c73b89bce02cba515cc
sawako osada <mugiwaranana@yahoo.co.jp>
2020-10-10 17:09:03 +0000

    html修正

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,6 @@
 class Property < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality:only_integer
+  validates :address, presence: true
+  validates :years, presence: true, numericality:only_integer
 end
